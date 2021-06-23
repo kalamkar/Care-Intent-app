ef1cb142e81bbac4bea2a8db472d10870d642908
abhi <abhi@kali.care>
2021-06-23 06:33:13 -0700

    Added basic theme and structure to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import {HttpClientModule} from "@angular/common/http";
 import {Ng2GoogleChartsModule} from "ng2-google-charts";
 import {AppGoogleChartComponent} from "./google-chart/app-google-chart.component";
 import { OverlayChartComponent } from './overlay-chart/overlay-chart.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -23,7 +28,12 @@ import { OverlayChartComponent } from './overlay-chart/overlay-chart.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    Ng2GoogleChartsModule
+    Ng2GoogleChartsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
